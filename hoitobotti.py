81863c5a7c28ee2f0c1c36fe984102c4098d2298
Deldmon <127428932+Deldmon@users.noreply.github.com>
2025-04-06 17:53:04 +0300

    Update hoitobotti.py

--- hoitobotti.py
+++ hoitobotti.py
@@ -1,3 +1,5 @@
 print("Hei. Olen hoitobotti. Ketä saan hoitaa tänään?")
+print("Ja autan sinua hoitoon liittyvissä asioissa.")
 name = input("Annanimesi: ")
-print(f"Tervetuloa vastaanotolle, {name}!")
+print(f"Tervetuloa vastaanotolle, {name}! ")
+print("Kuinka voin auttaa sinua ? ")
